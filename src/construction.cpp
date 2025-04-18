83110a3e85a1dcf524917d9765d2d04c4c5384fc
gsherterton <gsherterton@gmail.com>
2025-04-17 22:26:55 -0300

    Adicionando randomidade no construtor

--- src/construction.cpp
+++ src/construction.cpp
@@ -7,6 +7,7 @@
 #include <cstddef>
 #include <cstdint>
 #include <functional>
+#include <iostream>
 
 struct Insertion {
     size_t candidate_i;
@@ -115,12 +116,16 @@ Solution ASP::lowest_release_time_insertion(std::vector<Flight> &flights) {
         return flight_a.get().get_release_time() > flight_b.get().get_release_time();
     });
 
+    size_t qtd_runways = m_instance.get_num_runways();
+
     // Put the "runway.size()"'s lowests release time flights
     for (size_t runway_i = 0; runway_i < m_instance.get_num_runways(); ++runway_i) {
-        solution.runways[runway_i].sequence.emplace_back(candidate_list.back()); // Coloca ele no final daquela pista
+        size_t idx =  (rand() % std::min(qtd_runways, candidate_list.size())) + (candidate_list.size() - std::min(qtd_runways, candidate_list.size())); //index of the selected flight in candidate list
+
+        solution.runways[runway_i].sequence.emplace_back(candidate_list[idx]); // Coloca ele no final daquela pista
         solution.runways[runway_i].prefix_penalty.push_back(0); // O penalty dele é zero pois é o primeiro
 
-        Flight &candidate = candidate_list.back().get(); // Facilitar a escrita
+        Flight &candidate = candidate_list[idx].get(); // Facilitar a escrita
 
         // Atualiza seus valores
         candidate.position = 0;
@@ -128,6 +133,9 @@ Solution ASP::lowest_release_time_insertion(std::vector<Flight> &flights) {
         candidate.start_time = candidate.get_release_time();
 
         // Apaga ele da lista de candidatos
+        for(size_t i = idx; i < candidate_list.size()-1; i++){
+            candidate_list[i] = candidate_list[i+1];
+        }
         candidate_list.pop_back();
     }
 
@@ -141,7 +149,8 @@ Solution ASP::lowest_release_time_insertion(std::vector<Flight> &flights) {
         best_runway = 0;
         lowest_start_time = std::numeric_limits<size_t>::max();
 
-        const Flight &current_flight = candidate_list.back().get(); // the flight who will be insert
+        size_t idx =  (rand() % std::min(qtd_runways, candidate_list.size())) + (candidate_list.size() - std::min(qtd_runways, candidate_list.size())); //index of the selected flight in candidate list
+        const Flight &current_flight = candidate_list[idx].get(); // the flight who will be insert
 
         for (size_t runway_i = 0; runway_i < m_instance.get_num_runways(); ++runway_i) {
             const Flight &prev_flight =
@@ -165,9 +174,9 @@ Solution ASP::lowest_release_time_insertion(std::vector<Flight> &flights) {
                 .prefix_penalty[solution.runways[best_runway].sequence.size()] // the prefix of the last flight in the
                                                                                // runwawy
             + current_flight_penalty); // O prefix penalty dele é o prefix penalty do anterior mais o penalty dele
-        solution.runways[best_runway].sequence.emplace_back(candidate_list.back()); // Coloca ele no final daquela pista
+        solution.runways[best_runway].sequence.emplace_back(candidate_list[idx]); // Coloca ele no final daquela pista
 
-        Flight &candidate = candidate_list.back().get(); // Facilitar a escrita
+        Flight &candidate = candidate_list[idx].get(); // Facilitar a escrita
 
         // Atualiza seus valores
         candidate.position = solution.runways[best_runway].sequence.size() - 1;
@@ -178,6 +187,9 @@ Solution ASP::lowest_release_time_insertion(std::vector<Flight> &flights) {
         solution.objective += current_flight_penalty;
 
         // Apaga ele da lista de candidatos
+        for(size_t i = idx; i < candidate_list.size()-1; i++){
+            candidate_list[i] = candidate_list[i+1];
+        }
         candidate_list.pop_back();
     }
 
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 #include "solution.hpp"
 
 int main(int argc, char *argv[]) {
+    srand(time(NULL));
+
     argparse::ArgumentParser program("ASP");
 
     program.add_argument("instance").help("Path to the input file").required();
@@ -28,12 +30,14 @@ int main(int argc, char *argv[]) {
 
     ASP asp(instance);
 
-    Solution s2 = asp.GILS_VND_2(20, 30, 0.05);
-    s2.print();
+    // Solution s2 = asp.GILS_VND_2(20, 30, 0.05);
+    // s2.print();
 
     // Solution s1 = asp.GILS_VND(1, 50, 0);
     // s1.print();
 
+    Solution s1 = asp.lowest_release_time_insertion(asp.flights);
+    s1.print();
 
     return 0;
 }
