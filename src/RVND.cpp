d9e966d89f940c15e4e1904c6609c665c63b7bdd
GabrielCFormiga <gabriel.campfor@gmail.com>
2025-04-25 01:37:38 -0300

    feat: first_improvement_neighborhoods

--- src/RVND.cpp
+++ src/RVND.cpp
@@ -3,7 +3,7 @@
 
 void ASP::RVND(Solution &solution) { // NOLINT
     std::vector<Neighborhood> neighborhoods{Neighborhood::IntraSwap, Neighborhood::InterSwap, Neighborhood::IntraMove,
-                                            Neighborhood::InterMove/* , Neighborhood::WorstFlight */};
+                                            Neighborhood::InterMove};
 
     bool improved = false;
     size_t current_neighborhood = 0;
@@ -12,24 +12,25 @@ void ASP::RVND(Solution &solution) { // NOLINT
         current_neighborhood = rand() % neighborhoods.size();
         switch (neighborhoods[current_neighborhood]) {
         case Neighborhood::IntraSwap:
-            improved = best_improvement_intra_swap(solution);
+            // improved = best_improvement_intra_swap(solution);
+            improved = first_improvement_intra_swap(solution);
             break;
         case Neighborhood::InterSwap:
-            improved = best_improvement_inter_swap(solution);
+            // improved = best_improvement_inter_swap(solution);
+            improved = first_improvement_inter_swap(solution);
             break;
         case Neighborhood::IntraMove:
-            improved = best_improvement_intra_move(solution);
+            // improved = best_improvement_intra_move(solution);
+            improved = first_improvement_intra_move(solution);
             break;
         case Neighborhood::InterMove:
-            improved = best_improvement_inter_move(solution);
+            // improved = best_improvement_inter_move(solution);
+            improved = first_improvement_inter_move(solution);
             break;
-        /* case Neighborhood::WorstFlight:
-            improved = move_worst_flight(solution);
-            break; */
         }
         if (improved) {
             neighborhoods = {Neighborhood::IntraSwap, Neighborhood::InterSwap, Neighborhood::IntraMove,
-                             Neighborhood::InterMove/* , Neighborhood::WorstFlight */};
+                             Neighborhood::InterMove};
 
         } else {
             neighborhoods.erase(neighborhoods.begin() + static_cast<long>(current_neighborhood));
